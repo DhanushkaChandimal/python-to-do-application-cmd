130e4a415eed9c71a4f262e204e29704d300beff
Dhanushka <dhanushkamcr@gmail.com>
2025-08-10 11:34:34 -0500

    Display menu with options and implement quit application

--- a.py
+++ a.py
@@ -1,6 +1,16 @@
-print("""                           Hi Welcome to,
-             //////////     ///////              |//////      ///////
-                ///       ///    ///             |||  ///   ///    ///
-               ///        ///   ///              |||  ///   ///   /// 
-              ///         //////                 |//////    //////
-                                    Application""")
+print("""                               Hi Welcome to,
+     //////////     ///////              |//////      ///////
+        ///       ///    ///             |||  ///   ///    ///
+       ///        ///   ///              |||  ///   ///   /// 
+      ///         //////                 |//////    //////
+                           Application""")
+
+isEnd = False
+while(not isEnd):
+    user_input=input("""➕Add task(a/add)
+📋View tasks(v/view)
+🗑️Delete task(d/delete)
+🛑Quit application(q/quit)
+Please enter your input here: """)
+    if(user_input == "q" or "quit"):
+        isEnd = True
