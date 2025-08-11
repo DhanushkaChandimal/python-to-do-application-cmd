e60dfda6e40e2d21045cb3f772977166aeaac52e
Dhanushka <dhanushkamcr@gmail.com>
2025-08-11 10:21:27 -0500

    Implemented add task method

--- a.py
+++ a.py
@@ -1,3 +1,5 @@
+task_list = []
+
 print("""                               Hi Welcome to,
      //////////     ///////              |//////      ///////
         ///       ///    ///             |||  ///   ///    ///
@@ -5,12 +7,29 @@ print("""                               Hi Welcome to,
       ///         //////                 |//////    //////
                            Application""")
 
+def add_task(task):
+    task_list.append(task)
+
 isEnd = False
 while(not isEnd):
-    user_input=input("""➕Add task(a/add)
-📋View tasks(v/view)
-🗑️Delete task(d/delete)
-🛑Quit application(q/quit)
-Please enter your input here: """)
-    if(user_input == "q" or "quit"):
+    user_input=input("""\n➕ Add task (a / add)
+📋 View task_list (v / view)
+🗑️  Delete task (d / delete)
+🛑 Quit application (q / quit)
+
+Please enter your input here: """).lower()
+    if(user_input == "q" or user_input == "quit"):
         isEnd = True
+        print("---------------------")
+        print(user_input)
+        print(user_input=="q")
+        print(user_input == "q" or "quit")
+    elif(user_input == "a" or "add"):
+        while(True):
+            user_input=input(f"\nEnter your task {len(task_list)+1}: ")
+            if(len(user_input) < 3):
+                print("Error: Minimum characters should be 3!")
+                continue
+            else:
+                add_task(user_input)
+                break
