3df491286f86be257d34b608e4653197bff46252
Dhanushka <dhanushkamcr@gmail.com>
2025-08-11 10:37:51 -0500

    Implemented view tasks method

--- a.py
+++ a.py
@@ -10,6 +10,15 @@ print("""                               Hi Welcome to,
 def add_task(task):
     task_list.append(task)
 
+def view_tasks():
+    if(len(task_list)<=0):
+        return "\n                    To do list is empty!"
+    else:
+        task_table = ""
+        for index in range(len(task_list)):
+            task_table += f"\n                    {index+1} - {task_list[index]}"
+        return task_table
+
 isEnd = False
 while(not isEnd):
     user_input=input("""\n➕ Add task (a / add)
@@ -20,16 +29,16 @@ while(not isEnd):
 Please enter your input here: """).lower()
     if(user_input == "q" or user_input == "quit"):
         isEnd = True
-        print("---------------------")
-        print(user_input)
-        print(user_input=="q")
-        print(user_input == "q" or "quit")
-    elif(user_input == "a" or "add"):
+    elif(user_input == "a" or user_input == "add"):
         while(True):
             user_input=input(f"\nEnter your task {len(task_list)+1}: ")
             if(len(user_input) < 3):
-                print("Error: Minimum characters should be 3!")
+                print("                    Error: Minimum characters should be 3!")
                 continue
             else:
                 add_task(user_input)
                 break
+    elif(user_input == "v" or user_input == "view"):
+        print(view_tasks())
+    else:
+        print("\n                    Invalid input! Please enter input from above list.")
